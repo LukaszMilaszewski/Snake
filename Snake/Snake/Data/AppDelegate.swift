7f995075868c90cef8a9f0b87b1d4204eff5801a
LukaszMilaszewski <lukasz.milaszewski@gmail.com>
2018-04-27 17:16:36 +0200

    Removed storyboards

--- Snake/Snake/Data/AppDelegate.swift
+++ Snake/Snake/Data/AppDelegate.swift
@@ -6,7 +6,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    window = UIWindow(frame: UIScreen.main.bounds)
+    let homeViewController = GameViewController()
+    window!.rootViewController = homeViewController
+    window!.makeKeyAndVisible()
     return true
   }
 }
